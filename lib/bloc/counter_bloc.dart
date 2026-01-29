e612c7df29dc29d980d22f65f606f436c99788e7
Roman-Dewan <mdromandewan1888@gmail.com>
2026-01-29 16:34:21 +0600

    flutter bloc-beginer level complete

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-sealed class CounterEvent{}
-class CounterIncremented extends CounterEvent{}
-class CounterDecremented extends CounterEvent{}
+part 'counter_event.dart';
 
 class CounterBloc extends Bloc<CounterEvent, int>{
   CounterBloc() : super(0){
