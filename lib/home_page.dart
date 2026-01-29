de05caf764d916d7a5d6b135b060f15ee25ca031
Roman-Dewan <mdromandewan1888@gmail.com>
2026-01-29 11:51:31 +0600

    bloc builder practise done.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test03/cubit/counter_cubit.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -14,33 +15,34 @@ class _MyHomePageState extends State<MyHomePage> {
   final counterCubit = CounterCubit();
   @override
   Widget build(BuildContext context) {
-    final counter = counterCubit.state;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return BlocBuilder<CounterCubit, int>(
+      bloc: counterCubit,
+      builder: (context, counter) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+            title: Text(widget.title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('You have pushed the button this many times:'),
+                Text(
+                  '$counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:() {
-          counterCubit.increment();
-          setState(() {});
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed:() => counterCubit.increment(),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        );
+      }
     );
   }
 }
